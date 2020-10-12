f4c0a1114b7083b7424649896d063fab7e10b6bc
Ariel Wainer <arielwainer4@gmail.com>
2020-10-12 16:48:25 -0400

    oct 12 leetcode challenge + line explanations

--- October Challenge/10-12.js
+++ October Challenge/10-12.js
@@ -8,14 +8,26 @@
  * @return {boolean}
  */
 var buddyStrings = function(A, B) {
+  // if A and B are different lengths, can't swap to make equal strings
   if (A.length != B.length) {
       return false;
   }
-  let [dif, chars] = [[], new Set(A)];
+
+  // dif = when characters in A and B at the same index dont match
+  // chars = deduped list of characters in A
+
+  let dif = []
+  let chars = new Set(A);
+  // loop through A, if chars dont match, push the pair to dif
   for (i in A) {
       if (A[i] != B[i]) {
           dif.push([A[i], B[i]]);
       }
   }
+  // return has 2 parts:
+
+  // if there are 2 pairs in dif and dif[0] = reverse of dif[1] return true
+
+  // if dif is empty (meaning stirngs match) and num of characters !== length (theres duplicate letters) return true
   return dif.length == 2 && dif[0].join() == dif[1].reverse().join() || (dif.length == 0 && chars.size != A.length);
 };
