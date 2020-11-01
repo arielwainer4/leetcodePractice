0d621f725b21c3eacacf1d50d5d36bb8f217521d
Ariel Wainer <arielwainer4@gmail.com>
2020-11-01 11:47:50 -0500

    leetcode challenge

---
+++ November Challenge/11-1b.js
@@ -0,0 +1,29 @@
+// Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+
+//  Time: O(n) n = number of nodes
+// Space: O(n)
+var getDecimalValue = function(head) {
+  let num = []
+
+  while(head) {
+      num.push(head.val)
+      head = head.next
+  }
+
+  num = num.join('')
+  return parseInt(num,2)
+};
