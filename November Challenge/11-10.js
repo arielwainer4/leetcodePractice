5c20636dab2ce0f7fa4e5db98f5fbe84a9b99ec6
Ariel Wainer <arielwainer4@gmail.com>
2020-11-10 09:39:56 -0500

    leetcode daily

---
+++ November Challenge/11-10.js
@@ -0,0 +1,24 @@
+// Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+
+// To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+// To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+// time: O(r*c)
+// space: O(r*c)
+var flipAndInvertImage = function(A) {
+  let res = []
+  for(let row = 0; row < A.length; row++) {
+      let newRow = []
+      for(let col = A[row].length-1; col >= 0; col--) {
+          if(A[row][col] === 0) newRow.push(1)
+          else newRow.push(0)
+      }
+      res.push(newRow)
+  }
+  return res
+};
